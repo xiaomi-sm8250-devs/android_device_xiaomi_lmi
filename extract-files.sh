ce32e540d0fc299f8856258eb636ff9b795e80d5
luffitys <banackidaniel@gmail.com>
2024-10-12 19:39:37 +0200

    lmi: extract-files: Fix patching audio.primary.lmi.so
    Change-Id: I07286ee500441f19ee4c0dec4e9f1620923e87bd

--- extract-files.sh
+++ extract-files.sh
@@ -23,7 +23,7 @@ EOF
             ;;
         vendor/lib/hw/audio.primary.lmi.so)
             [ "$2" = "" ] && return 0
-            sed -i "s|/vendor/lib/liba2dpoffload\.so|liba2dpoffload_lmi\.so\x00\x00\x00\x00\x00\x00\x00\x00" "${2}"
+            sed -i "s|/vendor/lib/liba2dpoffload\.so|liba2dpoffload_lmi\.so\x00\x00\x00\x00\x00\x00\x00\x00|g" "${2}"
             ;;
         vendor/lib64/camera/components/com.mi.node.watermark.so)
             [ "$2" = "" ] && return 0
